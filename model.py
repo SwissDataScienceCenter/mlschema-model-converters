33c04ee98fd847ff114db7b5de54fb1d8d7fe4b1
Christopher Barber <christopher.barber@inf.ethz.ch>
2019-12-10 15:47:57 +0100

    convert sklearn model params to mlschema model

---
+++ model.py
@@ -0,0 +1,19 @@
+from renku.core.models import mls
+from rdflib import Literal
+import sklearn
+
+def to_mls(sklearn_model: sklearn.base.BaseEstimator):
+    params = sklearn_model.get_params()
+    model = mls.Run()
+
+    implementation = mls.Implementation(id=type(sklearn_model))
+    implementation.parameters = [mls.HyperParameter(id=key) for key in params.keys()]
+    implementation.implements = mls.Algorithm(id=type(sklearn_model))
+    model.executes = implementation
+
+    model.input_values = [
+        mls.HyperParameterSetting(value=val, specified_by=key)
+        for key, val in params.items() if val is not None
+    ]
+
+    return model.asjsonld()
