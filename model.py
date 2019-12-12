62b34850c325ea0c7f77f9180133168bc51f02d9
Christopher Barber <christopher.barber@inf.ethz.ch>
2019-12-12 11:01:05 +0100

    fix converting model type to string

--- model.py
+++ model.py
@@ -4,11 +4,13 @@ import sklearn
 
 def to_mls(sklearn_model: sklearn.base.BaseEstimator):
     params = sklearn_model.get_params()
+    model_class = type(sklearn_model).__module__ + '.' + type(sklearn_model).__name__
+
     model = mls.Run()
 
-    implementation = mls.Implementation(id=type(sklearn_model))
+    implementation = mls.Implementation(id=model_class)
     implementation.parameters = [mls.HyperParameter(id=key) for key in params.keys()]
-    implementation.implements = mls.Algorithm(id=type(sklearn_model))
+    implementation.implements = mls.Algorithm(id=model_class)
     model.executes = implementation
 
     model.input_values = [
