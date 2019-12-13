8621c43d9a7c270d6831bf85cb600d66ff546c61
Christopher Barber <christopher.barber@inf.ethz.ch>
2019-12-13 14:46:16 +0100

    deep calling of get_params(), add a test

--- model.py
+++ model.py
@@ -1,9 +1,28 @@
 from renku.core.models import mls
 from rdflib import Literal
 import sklearn
+import json
 
 def to_mls(sklearn_model: sklearn.base.BaseEstimator):
     params = sklearn_model.get_params()
+
+    def deep_get_params(params):
+        d = {}
+        for k, v in params.items():
+            try:
+                p = v.get_params()
+                t = type(v).__module__ + '.' + type(v).__name__
+                d[k] = {'@value': {'type': t, 'params': deep_get_params(p)}}
+            except AttributeError:
+                try:
+                    json.dumps(v)
+                    d[k] = v
+                except:
+                    d[k] = str(v)
+        return d
+
+    params = deep_get_params(params)
+
     model_class = type(sklearn_model).__module__ + '.' + type(sklearn_model).__name__
 
     model = mls.Run()
---
+++ test_model.py
@@ -0,0 +1,67 @@
+import pytest
+from model import to_mls
+import json
+import itertools
+
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV, \
+    PassiveAggressiveClassifier, Perceptron, RidgeClassifier, RidgeClassifierCV, \
+    SGDClassifier, LinearRegression, Ridge, RidgeCV, SGDRegressor, ElasticNet, ElasticNetCV, \
+    Lars, LarsCV, Lasso, LassoCV, LassoLars, LassoLarsCV, LassoLarsIC, OrthogonalMatchingPursuit, \
+    OrthogonalMatchingPursuitCV, ARDRegression, BayesianRidge, HuberRegressor, RANSACRegressor, \
+    TheilSenRegressor, PassiveAggressiveRegressor
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+@pytest.mark.parametrize("sklearn_model", [
+    LogisticRegression(random_state=0),
+    LogisticRegressionCV(cv=5, random_state=0),
+    PassiveAggressiveClassifier(max_iter=1000, random_state=0, tol=1e-3),
+    Perceptron(tol=1e-3, random_state=0),
+    RidgeClassifier(),
+    RidgeClassifierCV(alphas=[1e-3, 1e-2, 1e-1, 1]),
+    SGDClassifier(max_iter=1000, tol=1e-3),
+    LinearRegression(),
+    Ridge(),
+    RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1]),
+    SGDRegressor(max_iter=1000, tol=1e-3),
+    ElasticNet(random_state=0),
+    ElasticNetCV(cv=5, random_state=0),
+    Lars(n_nonzero_coefs=1),
+    LarsCV(cv=5),
+    Lasso(alpha=0.1),
+    LassoCV(cv=5, random_state=0),
+    LassoLars(alpha=0.01),
+    LassoLarsCV(cv=5),
+    LassoLarsIC(criterion='bic'),
+    OrthogonalMatchingPursuit(),
+    OrthogonalMatchingPursuitCV(cv=5),
+    ARDRegression(),
+    BayesianRidge(),
+    HuberRegressor(),
+    RANSACRegressor(random_state=0),
+    TheilSenRegressor(random_state=0),
+    PassiveAggressiveRegressor(max_iter=100, random_state=0, tol=1e-3),
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=2, C=1),
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    MLPClassifier(alpha=1, max_iter=1000),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis(),
+])
+def test_to_mls(sklearn_model):
+    sklearn_model.fit(
+        [[i+j, i+j] for i, j in itertools.product(range(5), range(3))],
+        list(itertools.chain.from_iterable(itertools.repeat([0, 1, 2], 5)))
+    )
+    json.dumps(to_mls(sklearn_model))
